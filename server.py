affac957bee621c83713e358e29f93405c657117
MissHela <weiwang9807@outlook.com>
2022-04-12 00:18:04 -0400

    conflicts

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response,flash, session, abort
+
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -12,6 +13,16 @@ DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
 
 engine = create_engine(DATABASEURI)
+
+
+# Here we create a test table and insert some values in it
+engine.execute("""DROP TABLE IF EXISTS test;""")
+engine.execute("""CREATE TABLE IF NOT EXISTS test (
+  id serial,
+  name text
+);""")
+engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+
 
 @app.before_request
 def before_request():
@@ -33,6 +44,12 @@ def teardown_request(exception):
 def index():
   print (request.args)
   cursor = g.conn.execute("SELECT name FROM test")
+
+
+  #
+  # example of a database query
+  #
+  cursor = g.conn.execute("SELECT name FROM U")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
@@ -78,7 +95,39 @@ def add():
   return redirect('/')
 
 
+@app.route('/home')
+def home():
+  if not session.get('logged_in'):
+    return render_template('login.html')
+  else:
+    return "Hello Boss!"
+
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+  password_prime = request.form['password']
+  username_prime = request.form['username']
+
+  cmd = 'SELECT uid FROM U WHERE name = (:n1)';
+
+  cursor = g.conn.execute(text(cmd), n1 = username_prime);
+  password = []
+  for result in cursor:
+    password.append(result['uid'])  # can also be accessed using result[0]
+  cursor.close()
+
+  if password_prime == str(password[0]) :
+    session['logged_in'] = True 
+  else:
+    flash('wrong password!')
+  return redirect('/home')
+
+
 if __name__ == "__main__":
+
+  app.secret_key = os.urandom(12)
+
   import click
 
   @click.command()
